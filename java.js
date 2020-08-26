77589e188de80fedd1acf06a37cef77f2ee8967a
batoolalomari <batoolalomari2@gmail.com>
2020-08-26 13:03:33 +0300

    add function to js file

--- java.js
+++ java.js
@@ -3,13 +3,21 @@ var books
 var greeting
 var lName
 
-fName =prompt("enter your First name");
-lName =prompt("enter your Last name");
-books=prompt("How many books do you read a weak?");
-document.write("Hey, "+ fName +"  "+ lName+ ", how are you today?");
+fName =prompt("Enter your First name");
+lName =prompt("Enter your Last name");
+books=prompt("How many books do you read a weak ? " );
+
+document.write( '<h3 >' +" "+"Hey, "+ fullName(fName,lName)+" "+ '</h3>');
+var x=Books(books)
+//document.write(x);
 
 
 
+
+function Books (books)
+{
+    if(books>=0)
+    {
 if (books> 10) {
    greeting= 'Great Job!'; 
 }
@@ -17,6 +25,18 @@ if (books> 10) {
 else {
     greeting='Try harder';
 }
-document.write('<h3>' + greeting+ '</h3>');
+    }
+    else
+    greeting='Please Enter positive number';
 
 
+    document.write('<h3 >' +" "+ greeting+" "+ '</h3>');
+    return greeting;
+
+}
+
+function fullName( fName,lName)
+{
+
+return fName +"  " +lName;
+}
