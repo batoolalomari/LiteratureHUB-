f91a911f984794b303463358889dd8df4ed67e62
batoolalomari <batoolalomari2@gmail.com>
2020-08-27 10:08:56 +0300

    to be more good

--- java.js
+++ java.js
@@ -2,21 +2,24 @@ var fName
 var books 
 var greeting
 var lName
-
+var cat=""
 fName =prompt("Enter your First name");
 lName =prompt("Enter your Last name");
 books=prompt("How many books do you read a weak ? " );
 
-document.write( '<h3 >' +" "+"Hey, "+ fullName(fName,lName)+" "+ '</h3>');
-var x=Books(books)
-//document.write(x);
 
+
+
+document.write( '<h3 >' +" "+"Hey, "+ fullName(fName,lName)+" "+ '</h3>');
+
+//document.write(x);
+document.write(addCtegName(cat) );
 
 
 
 function Books (books)
 {
-    if(books>=0)
+    if(books>=0 && isNaN(books))
     {
 if (books> 10) {
    greeting= 'Great Job!'; 
@@ -39,4 +42,27 @@ function fullName( fName,lName)
 {
 
 return fName +"  " +lName;
+}
+
+
+
+function addCtegName( cat)
+{
+   var  catNum=prompt("how many category do want to add " );
+    
+   
+   for(var i=0;i<catNum;i++)
+    {
+        cat=prompt("Enter the category name " );
+       
+        console.log(cat);
+
+        /*catName=prompt("Enter the category name " );
+        cat =catName;
+        console.log(catName);*/
+        document.write('<p >' +" "+ i+cat +" "+ '<p>');
+    }
+
+    return cat;
+    
 }
